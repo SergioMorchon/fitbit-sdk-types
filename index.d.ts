84977f5917d5b6b60660483220d6e7018defdb23
Sergio Morchón Poveda <sergio.morchon@outlook.com>
2018-10-07 20:48:41 +0200

    Add system

--- index.d.ts
+++ index.d.ts
@@ -455,3 +455,25 @@ declare module 'power' {
 	}
 	export const charger: Charger;
 }
+
+declare module 'system' {
+	interface EventPressureMonitorEventMap {
+		memorypressurechange: Event;
+	}
+	interface MemoryPressureMonitor
+		extends StrictEventListener<EventPressureMonitorEventMap> {
+		onmemorypressurechange: (event: Event) => void;
+		readonly pressure: 'normal' | 'high' | 'critical';
+	}
+	interface MemoryUsage {
+		readonly peak: number;
+		readonly total: number;
+		readonly used: number;
+	}
+	interface Memory {
+		readonly js: MemoryUsage;
+		readonly monitor: MemoryPressureMonitor;
+		readonly native: MemoryUsage;
+	}
+	export const memory: Memory;
+}
---
+++ test/system/memory.ts
@@ -0,0 +1,2 @@
+import { memory } from 'system';
+console.log('JS memory: ' + memory.js.used + '/' + memory.js.total);
