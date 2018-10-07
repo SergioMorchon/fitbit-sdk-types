0e54f5f86225d00f3c6eb04195a2ca3e66067a34
Sergio Morchón Poveda <sergio.morchon@outlook.com>
2018-10-07 16:25:44 +0200

    Plain dts

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,50 @@
-/// <reference path="./src/device/sensor.d.ts" />
+type PermissionName =
+	| 'access_activity'
+	| 'access_user_profile'
+	| 'access_heart_rate'
+	| 'access_location'
+	| 'access_internet'
+	| 'run_background';
+
+interface Permissions {
+	granted(permissionName: PermissionName): boolean;
+}
+
+type BatchedReading<Reading> = { [P in keyof Reading]: Array<Reading[P]> };
+
+interface Sensor<Reading> {
+	readonly activated: boolean;
+	onactivate(event: Event): void;
+	onerror(event: SensorErrorEvent): void;
+	onreading(event: Event): void;
+	start(): void;
+	stop(): void;
+	readonly readings: BatchedReading<Reading>;
+}
+
+interface SensorErrorEvent {
+	readonly defaultPrevented: boolean;
+	readonly error: Error;
+	readonly target: EventTarget;
+	readonly type: string;
+}
+
+interface BatchedSensorReading {
+	readonly timestamp: Float32Array;
+}
+
+interface SensorReading {
+	readonly timestamp: number | null;
+}
+
+interface SensorOptions {
+	readonly frequency?: number | undefined;
+	readonly batch?: number | undefined;
+}
+
+interface BatchedSensorOptions extends SensorOptions {
+	readonly batch?: number | undefined;
+}
 
 declare module 'accelerometer' {
 	interface AccelerometerReading {
