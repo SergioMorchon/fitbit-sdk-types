4c1985bca0fccb2637f53b27ab7ed02ef960e882
Sergio Morchón Poveda <sergio.morchon@outlook.com>
2018-10-09 21:49:15 +0200

    Add Crypto

---
+++ test/device/crypto.ts
@@ -0,0 +1,12 @@
+// SHA-256 hash example
+import * as Crypto from "crypto";
+
+const buffer = new ArrayBuffer(3);
+const bytes = new Uint8Array(buffer);
+bytes[0] = 1;
+bytes[1] = 2;
+bytes[2] = 3;
+
+Crypto.subtle.digest("SHA-256", buffer).then(hash => {
+ console.log(hash);
+});
