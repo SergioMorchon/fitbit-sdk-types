9018f8ef687bb5e04d240cc8fa779c2ff82a1bc6
Sergio Morchón Poveda <sergio.morchon@outlook.com>
2018-10-21 16:45:21 +0200

    JSX

--- settings/components.d.ts
+++ settings/components.d.ts
@@ -1,141 +1,140 @@
-import { Component, ReactNode, SyntheticEvent } from 'react';
+type Component<Props> = (props: Props) => JSX.Element;
 
-declare global {
-	function Page(): Component;
-	function Section(): Component<{
-		title: ReactNode;
-		description?: ReactNode;
-	}>;
-	function Link(): Component<{
-		source: string;
-	}>;
-	function Text(): Component<{
-		bold?: boolean;
-		italic?: boolean;
-		align?: 'left' | 'center' | 'right' | string;
-	}>;
-	function TextImageRow(): Component<{
-		label?: string;
-		sublabel?: string;
-		icon?: string;
-	}>;
-	function Button(): Component<{
-		label?: string;
-		list?: boolean;
-		onClick: (event: SyntheticEvent) => void;
-	}>;
-	function Toggle(): Component<{
-		settingsKey: string;
-		label?: string;
-		onChange?: (newValue: boolean) => void;
-	}>;
-	function Slider(): Component<{
-		label?: string;
-		settingsKey?: string;
-		min: number | string;
-		max: number | string;
-		step?: number | string;
-		onChange?: (newValue: number) => void;
-	}>;
-	type AutocompleteOption = { name: string; [key: string]: any };
-	function TextInput(): Component<{
-		title?: string;
-		label?: string;
-		placeholder?: string;
-		action?: string;
-		type?: string;
-		disabled?: boolean;
-		settingsKey?: string;
-		onChange?: (newValue: string) => void;
-		onAutocomplete?: (newValue: string) => Array<AutocompleteOption>;
-		renderItem?: (autocompleteOption: AutocompleteOption) => JSX.Element;
-		renderAutocomplete?: (
-			autoCompleteOption: AutocompleteOption,
-			newValue: string,
-		) => JSX.Element;
-	}>;
-	function ColorSelect(): Component<{
-		settingsKey: string;
-		colors: Array<{
-			color: string;
-			value?: any;
-		}>;
-		centered?: boolean;
-		onSelection?: (value: string) => void;
-	}>;
-	type SelectOption = {
-		name: string;
+declare const Page: Component<{ children: JSX.Element }>;
+declare const Section: Component<{
+	title: JSX.Element;
+	description?: JSX.Element;
+}>;
+declare const Link: Component<{
+	source: string;
+}>;
+declare const Text: Component<{
+	bold?: boolean;
+	italic?: boolean;
+	align?: 'left' | 'center' | 'right';
+	children: JSX.Element;
+}>;
+declare const TextImageRow: Component<{
+	label?: string;
+	sublabel?: string;
+	icon?: string;
+}>;
+declare const Button: Component<{
+	label?: string;
+	list?: boolean;
+	onClick: (event: Event) => void;
+}>;
+declare const Toggle: Component<{
+	settingsKey: string;
+	label?: string;
+	onChange?: (newValue: boolean) => void;
+}>;
+declare const Slider: Component<{
+	label?: string;
+	settingsKey?: string;
+	min: number | string;
+	max: number | string;
+	step?: number | string;
+	onChange?: (newValue: number) => void;
+}>;
+type AutocompleteOption = { name: string; [key: string]: any };
+declare const TextInput: Component<{
+	title?: string;
+	label?: string;
+	placeholder?: string;
+	action?: string;
+	type?: string;
+	disabled?: boolean;
+	settingsKey?: string;
+	onChange?: (newValue: string) => void;
+	onAutocomplete?: (newValue: string) => Array<AutocompleteOption>;
+	renderItem?: (autocompleteOption: AutocompleteOption) => JSX.Element;
+	renderAutocomplete?: (
+		autoCompleteOption: AutocompleteOption,
+		newValue: string,
+	) => JSX.Element;
+}>;
+declare const ColorSelect: Component<{
+	settingsKey: string;
+	colors: Array<{
+		color: string;
 		value?: any;
-		[extraProperty: string]: any;
-	};
-	function Select(): Component<{
-		title?: string;
-		selectViewTitle?: string;
-		label?: string;
-		settingsKey?: string;
-		options: Array<SelectOption>;
-		multiple?: boolean;
-		disabled?: boolean;
-		renderItem?: (option: SelectOption) => JSX.Element;
-		onSelection?: (
-			selection: { selected: Array<number>; values: Array<any> },
-		) => void;
 	}>;
-	function AdditiveList(): Component<{
-		title?: string;
-		description?: string;
-		settingsKey?: string;
-		minItems?: number | string;
-		maxItems?: number | string;
-		renderItem?: (item: SelectOption) => JSX.Element;
-		addAction?: JSX.Element;
-		onListChange?: (elements: Array<SelectOption>) => void;
-	}>;
-	function Oauth(): Component<{
-		title?: string;
-		label?: string;
-		status?: string;
-		description?: string;
-		settingsKey?: string;
-		authorizeUrl?: string;
-		requestTokenUrl?: string;
-		clientId?: string;
-		clientSecret?: string;
-		scope?: string;
-		pkce?: boolean;
-		onAccessToken?: (accessToken: string) => void;
-	}>;
-	function StravaLogin(): Component<{
-		title?: string;
-		description?: string;
-		settingsKey?: string;
-		clientId?: string;
-		clientSecret?: string;
-		onAccessToken?: (accessToken: string, userInfo: any) => void;
-	}>;
-	function ImagePicker(): Component<{
-		title?: string;
-		description?: string;
-		label?: string;
-		sublabel?: string;
-		pickerTitle?: string;
-		pickerImageTitle?: string;
-		pickerLabel?: string;
-		settingsKey?: string;
-		imageWidth?: number | string;
-		imageHeight?: number | string;
-		showIcon?: boolean;
-		disabled?: boolean;
-		onImagePicked?: (
-			image: {
-				imageUri: string;
-				imageString: string;
-				type: string;
-				width: number;
-				height: number;
-			},
-		) => void;
-	}>;
+	centered?: boolean;
+	onSelection?: (value: string) => void;
+}>;
+type SelectOption = {
+	name: string;
+	value?: any;
+	[extraProperty: string]: any;
+};
+declare const Select: Component<{
+	title?: string;
+	selectViewTitle?: string;
+	label?: string;
+	settingsKey?: string;
+	options: Array<SelectOption>;
+	multiple?: boolean;
+	disabled?: boolean;
+	renderItem?: (option: SelectOption) => JSX.Element;
+	onSelection?: (
+		selection: { selected: Array<number>; values: Array<any> },
+	) => void;
+}>;
+declare const AdditiveList: Component<{
+	title?: string;
+	description?: string;
+	settingsKey?: string;
+	minItems?: number | string;
+	maxItems?: number | string;
+	renderItem?: (item: SelectOption) => JSX.Element;
+	addAction?: JSX.Element;
+	onListChange?: (elements: Array<SelectOption>) => void;
+}>;
+declare const Oauth: Component<{
+	title?: string;
+	label?: string;
+	status?: string;
+	description?: string;
+	settingsKey?: string;
+	authorizeUrl?: string;
+	requestTokenUrl?: string;
+	clientId?: string;
+	clientSecret?: string;
+	scope?: string;
+	pkce?: boolean;
+	onAccessToken?: (accessToken: string) => void;
+}>;
+declare const StravaLogin: Component<{
+	title?: string;
+	description?: string;
+	settingsKey?: string;
+	clientId?: string;
+	clientSecret?: string;
+	onAccessToken?: (accessToken: string, userInfo: any) => void;
+}>;
+declare const ImagePicker: Component<{
+	title?: string;
+	description?: string;
+	label?: string;
+	sublabel?: string;
+	pickerTitle?: string;
+	pickerImageTitle?: string;
+	pickerLabel?: string;
+	settingsKey?: string;
+	imageWidth?: number | string;
+	imageHeight?: number | string;
+	showIcon?: boolean;
+	disabled?: boolean;
+	onImagePicked?: (
+		image: {
+			imageUri: string;
+			imageString: string;
+			type: string;
+			width: number;
+			height: number;
+		},
+	) => void;
+}>;
 
-	function registerSettingsPage(component: () => JSX.Element): void;
-}
+declare function registerSettingsPage(element: JSX.Element): void;
--- test/settings/text.tsx
+++ test/settings/text.tsx
@@ -1,5 +1,5 @@
 <Text>Your Text Here</Text>;
-<Text align="right">This text will align to the right side</Text>;
+<Text align="center">This text will align to the right side</Text>;
 <Text italic>This text is italicized.</Text>;
 <Text align="center">
 	This text will be centered and <Text bold>bold</Text>
