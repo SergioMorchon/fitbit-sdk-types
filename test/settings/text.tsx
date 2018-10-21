1414f5aa5350a76a6495d0aec54992f2f1e0e6c7
Sergio Morchón Poveda <sergio.morchon@outlook.com>
2018-10-21 16:51:30 +0200

    Update text.tsx

--- test/settings/text.tsx
+++ test/settings/text.tsx
@@ -1,6 +1,6 @@
 <Text>Your Text Here</Text>;
-<Text align="center">This text will align to the right side</Text>;
+<Text align="right">This text will align to the right side</Text>;
 <Text italic>This text is italicized.</Text>;
-<Text align="right">
+<Text align="center">
 	This text will be centered and <Text bold>bold</Text>
 </Text>;
