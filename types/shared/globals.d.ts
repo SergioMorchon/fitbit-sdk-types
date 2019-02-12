6aa7ebc9eca50e8ac5e0f6ec95383f211fc97e5f
Sergio Morchón Poveda <sergio.morchon@outlook.com>
2019-02-12 22:46:32 +0100

    Latitude and longitude not null

--- types/shared/globals.d.ts
+++ types/shared/globals.d.ts
@@ -13,8 +13,8 @@ interface Coordinates {
 	readonly altitude: number | null;
 	readonly altitudeAccuracy: number | null;
 	readonly heading: number | null;
-	readonly latitude: number | null;
-	readonly longitude: number | null;
+	readonly latitude: number;
+	readonly longitude: number;
 	readonly speed: number | null;
 }
 interface Position {
