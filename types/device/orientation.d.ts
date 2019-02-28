7f05a20a504d8811f454e60bf69718d1a1de0e7d
Harry Eakins <harry@goklue.com>
2019-02-28 00:23:54 -0800

    Make Orientation's SensorReading interface be named more consistently with the other SensorReading extensions.

--- types/device/orientation.d.ts
+++ types/device/orientation.d.ts
@@ -1,12 +1,12 @@
 declare module 'orientation' {
 	type Quaternion = [number, number, number, number];
-	interface BatchReading extends SensorReading {
+	interface OrientationSensorReading extends SensorReading {
 		readonly i: number;
 		readonly j: number;
 		readonly k: number;
 		readonly scalar: number;
 	}
-	interface OrientationSensor extends Sensor<BatchReading> {
+	interface OrientationSensor extends Sensor<OrientationSensorReading> {
 		readonly quaternion: Quaternion | null;
 		readonly timestamp: number | null;
 	}
