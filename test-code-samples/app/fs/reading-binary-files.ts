281f9a7e820c495538972dac2c0331c03c7900f5
Sergio Morchón Poveda <sergio.morchon@outlook.com>
2019-02-28 20:10:55 +0100

    Fix linting

--- test-code-samples/app/fs/reading-binary-files.ts
+++ test-code-samples/app/fs/reading-binary-files.ts
@@ -7,7 +7,6 @@ console.log('bytes read: ', bytes_read);
 console.log('bytes:', bytes[0], bytes[1], bytes[2]);
 fs.closeSync(file);
 
-
 let buffer2 = fs.readFileSync('filename.bin');
 let bytes2 = new Uint8Array(buffer2);
 console.log('bytes: ', bytes2[0], bytes2[1], bytes2[2]);
