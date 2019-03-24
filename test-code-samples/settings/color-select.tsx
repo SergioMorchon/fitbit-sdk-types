7c45ba117336e6885dd64e9d10ca71f7e1201dd9
Sergio Morchón Poveda <sergio.morchon@outlook.com>
2019-03-24 13:37:21 +0100

    Generic Setting components

--- test-code-samples/settings/color-select.tsx
+++ test-code-samples/settings/color-select.tsx
@@ -1,13 +1,20 @@
 <ColorSelect
+	centered={true}
 	settingsKey="color"
 	colors={[
-		{ color: 'tomato' },
-		{ color: 'sandybrown' },
-		{ color: 'gold' },
-		{ color: 'aquamarine' },
-		{ color: 'deepskyblue' },
-		{ color: 'plum' },
+		{ color: 'tomato', value: '1' },
+		{ color: 'sandybrown', value: '2' },
+		{ color: 'gold', value: '3' },
+		{ color: 'aquamarine', value: '4' },
+		{ color: 'deepskyblue', value: '5' },
+		{ color: 'plum', value: '6' },
+		{ color: 'tomato', value: '7' },
+		{ color: 'sandybrown', value: '12' },
+		{ color: 'gold', value: '13' },
+		{ color: 'aquamarine', value: '14' },
+		{ color: 'deepskyblue', value: '15' },
 	]}
+	onSelection={value => console.log(value)}
 />;
 <ColorSelect
 	settingsKey="color"
@@ -15,12 +22,7 @@
 		{ color: 'tomato', value: { align: 'left', number: '1' } },
 		{ color: 'sandybrown', value: { align: 'right', number: '2' } },
 	]}
-/>;
-<ColorSelect
-	value={props.settings.color}
-	colors={[
-		{ color: 'tomato', value: { align: 'left', number: '1' } },
-		{ color: 'sandybrown', value: { align: 'right', number: '2' } },
-	]}
-	onSelection={value => props.settingsStorage.setItem('color', value)}
+	onSelection={value => {
+		console.log(`value: ${value.align}, number: ${value.number}`);
+	}}
 />;
