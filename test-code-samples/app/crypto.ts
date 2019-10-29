48f844ea5b293a897ef59751e011bac4e042a90a
Sergio Morchón Poveda <sergio.morchon@outlook.com>
2019-10-29 20:15:32 +0100

    Update Crypto

--- test-code-samples/app/crypto.ts
+++ test-code-samples/app/crypto.ts
@@ -1,15 +1,47 @@
-// SHA-256 hash example
 import * as Crypto from 'crypto';
 
-const buffer = new ArrayBuffer(3);
-const bytes = new Uint8Array(buffer);
-bytes[0] = 1;
-bytes[1] = 2;
-bytes[2] = 3;
+const buffer = new ArrayBuffer(16);
 
 Crypto.subtle.digest('SHA-256', buffer).then(hash => {
-	console.log(hash);
+	const h = new Uint8Array(hash);
 });
-Crypto.subtle.digest({ name: 'SHA-256', foo: 'bar' }, buffer).then(hash => {
-	console.log(hash);
-});
+
+const key = new ArrayBuffer(32);
+
+for (let i = 0; i < 32; i++) {
+	key[i] = i;
+}
+
+Crypto.subtle
+	.importKey('raw', key, { name: 'HMAC', hash: 'SHA-256' }, false, [
+		'sign',
+		'verify',
+	])
+
+	.then(secret => {
+		console.log(`Secret is: ${secret}`);
+
+		Crypto.subtle
+			.sign('HMAC', secret, buffer)
+			.then(signature => {
+				const sgn = new Uint8Array(signature);
+				console.log(`HMAC signature is: ${sgn}`);
+
+				Crypto.subtle
+					.verify('HMAC', secret, signature, buffer)
+					.then(success => {
+						console.log(success);
+						if (success) {
+							console.log('Signature was successfully verified!');
+						} else {
+							console.log('Bad signature!');
+						}
+					});
+			})
+			.catch(e => {
+				console.error(e);
+			});
+	})
+	.catch(e => {
+		console.error(e);
+	});
--- types/device/crypto.d.ts
+++ types/device/crypto.d.ts
@@ -1,14 +1,77 @@
 declare module 'crypto' {
-	type SHA_256 = 'SHA-256';
-	interface Algorithm {
-		name: SHA_256;
-		[key: string]: any;
+	type BufferSource =
+		| ArrayBuffer
+		| Int8Array
+		| Uint8Array
+		| Int16Array
+		| Uint16Array
+		| Int32Array
+		| Uint32Array;
+	type TypedArray =
+		| Int8Array
+		| Uint8Array
+		| Int16Array
+		| Uint16Array
+		| Int32Array
+		| Uint32Array;
+	type CryptoKeyUsage =
+		| 'encrypt'
+		| 'decrypt'
+		| 'sign'
+		| 'verify'
+		| 'deriveKey'
+		| 'deriveBits'
+		| 'wrapKey'
+		| 'unwrapKey';
+
+	interface AlgorithmAesGcm {
+		additionalData: BufferSource;
+		iv: BufferSource;
+		name: 'AES-GCM';
+		tagLength: number | void;
+	}
+
+	type Algorithm<T extends string> = T | { name: T };
+	interface KeyAlgorithm {
+		name: string;
+		hash: string | void;
+	}
+	class CryptoKey {
+		readonly algorithm: KeyAlgorithm;
+		readonly extractable: boolean;
+		readonly type: 'secret' | 'public' | 'private';
+		readonly usages: CryptoKeyUsage[];
 	}
 	interface SubtleCrypto {
+		decrypt(
+			algorithm: AlgorithmAesGcm,
+			key: CryptoKey,
+			data: BufferSource,
+		): Promise<ArrayBuffer>;
 		digest(
-			algorithm: SHA_256 | Algorithm,
-			data: ArrayBuffer | ArrayBufferView,
-		): Promise<string>;
+			algorithm: Algorithm<'SHA-256'>,
+			data: BufferSource,
+		): Promise<ArrayBuffer>;
+		exportKey(format: 'raw', key: CryptoKey): Promise<ArrayBuffer>;
+		importKey(
+			format: 'raw',
+			keyData: BufferSource,
+			algorithm: string | KeyAlgorithm,
+			extractable: boolean,
+			keyUsages: string[],
+		): Promise<CryptoKey>;
+		sign(
+			algorithm: Algorithm<'HMAC'>,
+			key: CryptoKey,
+			data: BufferSource,
+		): Promise<ArrayBuffer>;
+		verify(
+			algorithm: Algorithm<'HMAC'>,
+			key: CryptoKey,
+			signature: BufferSource,
+			data: BufferSource,
+		): Promise<boolean>;
 	}
 	const subtle: SubtleCrypto;
+	const getRandomValues: (values: TypedArray) => void;
 }
