c8c75d72426d1ce829e8d4194e617584d56db1a0
Bing Ren <bingtimren@gmail.com>
2020-05-31 15:59:03 +1000

    Relax 'value' in Storage API to 'any'

--- types/companion/storage.d.ts
+++ types/companion/storage.d.ts
@@ -1,8 +1,8 @@
 interface Storage {
 	readonly length: number;
 	clear(): void;
-	getItem(key: string): string | null;
+	getItem(key: string): any | null;
 	key(index: number): string | null;
 	removeItem(key: string): void;
-	setItem(key: string, value: string): void;
+	setItem(key: string, value: any): void;
 }
