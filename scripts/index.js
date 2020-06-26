c3868a14d92157438786ec031c074b7d6c49b6cb
Sergio Morchón Poveda <sergio.morchon@outlook.com>
2020-06-26 18:32:59 +0200

    Rename to ts file extension

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,33 @@
 
 const { name: moduleName, version: moduleVersion } = require('../package.json');
 const { execSync } = require('child_process');
-const { existsSync, writeFileSync, readFileSync } = require('fs');
+const {
+	existsSync,
+	writeFileSync,
+	readFileSync,
+	readdirSync,
+	statSync,
+	renameSync,
+} = require('fs');
 const { join } = require('path');
 
 const moduleDependency = `${moduleName}@${moduleVersion}`;
+
+/**
+ * @param {string} root
+ * @returns {Generator<string>}
+ */
+function* walkFiles(root) {
+	for (const fileName of readdirSync(root)) {
+		const path = join(root, fileName);
+		if (statSync(path).isDirectory()) {
+			yield* walkFiles(path);
+			continue;
+		}
+
+		yield path;
+	}
+}
 
 /**
  * @param {() => void} fn
@@ -13,11 +36,10 @@ const moduleDependency = `${moduleName}@${moduleVersion}`;
  */
 const tryRun = (fn, action) => {
 	try {
-		console.log(`Start ${action}`);
+		console.log(action);
 		fn();
-		console.log('Done');
 	} catch (error) {
-		console.error(`Error while ${action}: ${error}`);
+		console.error(`${action}: ${error}`);
 	}
 };
 
@@ -26,7 +48,7 @@ tryRun(
 		execSync(`npm install --save-dev ${moduleDependency}`, {
 			stdio: 'inherit',
 		}),
-	`Adding ${moduleDependency}`,
+	`Add dev dependency ${moduleDependency}`,
 );
 
 const tsconfigFileName = 'tsconfig.json';
@@ -39,6 +61,20 @@ for (const folder of ['app', 'companion', 'settings']) {
 	const targetFile = join(folder, tsconfigFileName);
 	tryRun(
 		() => writeFileSync(targetFile, readFileSync(sourceFile, 'utf-8')),
-		`Adding ${targetFile}`,
+		`Create ${targetFile}`,
 	);
+
+	for (const fileName of walkFiles(folder)) {
+		const result = /^(.*)\.js(x)?$/.exec(fileName);
+		if (!result) {
+			continue;
+		}
+
+		const [, name, x = ''] = result;
+		const targetName = `${name}.ts${x}`;
+		tryRun(
+			() => renameSync(fileName, targetName),
+			`Rename ${fileName} to ${targetName}`,
+		);
+	}
 }
