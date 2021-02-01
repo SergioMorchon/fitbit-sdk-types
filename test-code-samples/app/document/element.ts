91208784dcffdffef4a35ab9c1cb294279338905
Sergio Morchón Poveda <sergio.morchon@outlook.com>
2021-02-01 21:13:52 +0100

    Test element search selectors

---
+++ test-code-samples/app/document/element.ts
@@ -0,0 +1,6 @@
+declare const element: ElementSearch;
+
+element.getElementById('id') as Element;
+element.getElementsByClassName('class') as Element[];
+element.getElementsByTagName('tag') as Element[];
+element.getElementsByTypeName('type') as Element[];
