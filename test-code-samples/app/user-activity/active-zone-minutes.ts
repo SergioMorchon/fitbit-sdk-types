ba35b4428bd7ffe96b0cf653ceea9c7916e40ec7
Sergio Morchón Poveda <sergio.morchon@outlook.com>
2021-07-22 18:40:58 +0200

    AZM weekly goal

--- test-code-samples/app/user-activity/active-zone-minutes.ts
+++ test-code-samples/app/user-activity/active-zone-minutes.ts
@@ -1,4 +1,4 @@
-import { goals, today } from 'user-activity';
+import { goals, today, weekGoals, week } from 'user-activity';
 
 console.log(`${goals.activeZoneMinutes.total} activeZoneMinutes Goal`);
 
@@ -12,3 +12,8 @@ console.log(`${today.local.activeZoneMinutes.fatBurn}`);
 console.log(`${today.local.activeZoneMinutes.cardio}`);
 console.log(`${today.local.activeZoneMinutes.peak}`);
 console.log(`${today.adjusted.activeZoneMinutes.total}`);
+
+console.log(`${weekGoals.activeZoneMinutes} mins weekly AZM goal`);
+weekGoals.addEventListener('reachgoal', (evt) => {
+	// AZM Weekly Goal has been met
+});
