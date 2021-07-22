c09f5cb2c4977592b6190ba3924c65e859ae88b8
Sergio Morchón Poveda <sergio.morchon@outlook.com>
2021-07-22 18:30:24 +0200

    Add Weather

---
+++ test-code-samples/companion/weather.ts
@@ -0,0 +1,19 @@
+import { me as companion } from 'companion';
+import weather, { TemperatureUnit } from 'weather';
+
+if (companion.permissions.granted('access_location')) {
+	weather
+		.getWeatherData({ temperatureUnit: TemperatureUnit.Celsius })
+		.then((data) => {
+			if (data.locations.length > 0) {
+				const temp = Math.floor(data.locations[0].currentWeather.temperature);
+				const cond = data.locations[0].currentWeather.weatherCondition;
+				const loc = data.locations[0].name;
+				const unit = data.temperatureUnit;
+				console.log(`It's ${temp}° ${unit} and ${cond} in ${loc}`);
+			}
+		})
+		.catch((ex) => {
+			console.error(ex);
+		});
+}
